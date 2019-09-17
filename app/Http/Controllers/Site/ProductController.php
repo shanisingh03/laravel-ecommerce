6e77677109c3be66ecb592eefa8bd2cc8deb170b
Shani Singh <shani@torinit.ca>
2019-09-17 10:43:51 +0530

    Added product page

--- app/Http/Controllers/Site/ProductController.php
+++ app/Http/Controllers/Site/ProductController.php
@@ -4,21 +4,39 @@ namespace App\Http\Controllers\Site;
 
 use Illuminate\Http\Request;
 use App\Contracts\ProductContract;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\BaseController;
+use App\Contracts\AttributeContract;
 
-class ProductController extends Controller
+class ProductController extends BaseController
 {
     protected $productRepository;
 
-    public function __construct(ProductContract $productRepository)
+    protected $attributeRepository;
+
+    public function __construct(ProductContract $productRepository, AttributeContract $attributeRepository)
     {
         $this->productRepository = $productRepository;
+        $this->attributeRepository = $attributeRepository;
     }
 
+    /**
+     * Show Product Details
+     * Author: Shani Singh
+     */
     public function show($slug)
     {
         $product = $this->productRepository->findProductBySlug($slug);
+        $attributes = $this->attributeRepository->listAttributes();
 
-        dd($product);
+        return view('site.pages.product', compact('product', 'attributes'));
+    }
+
+    /**
+     * Add to Cart 
+     * Author: Shani Singh
+     */
+    public function addToCart(Request $request)
+    {
+        dd($request->all());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/category/{slug}', 'Site\CategoryController@show')->name('category.show');
 
 Route::get('/product/{slug}', 'Site\ProductController@show')->name('product.show');
+
+Route::post('/product/add/cart', 'Site\ProductController@addToCart')->name('product.add.cart');
